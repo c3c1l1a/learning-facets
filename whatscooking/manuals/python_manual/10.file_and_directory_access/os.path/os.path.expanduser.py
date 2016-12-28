4a5f686b915183b6b630eb5e0c40203fd3b84296
c3c1l1a <cesswangumukima@gmail.com>
2016-12-28 20:26:32 +0300

    os.path.expanduser manual

---
+++ whatscooking/manuals/python_manual/10.file_and_directory_access/os.path/os.path.expanduser.py
@@ -0,0 +1,8 @@
+from os.path import expanduser
+
+print expanduser('~') # Returns '/home/cess'
+print expanduser('~/test') # Returns '/home/cess/test'
+print expanduser('test/') # Returns 'test/'
+"""
+This function just exands the user. 
+"""
